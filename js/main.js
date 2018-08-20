6d177362a5261e818a9fa1ce1ff491b0514bfef4
unknown <ipowen10@gmail.com>
2018-08-19 22:38:24 -0400

    Update restaurant cards formatting

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiaXBvd2VuIiwiYSI6ImNqa3hjaWxxbjA3OWczdnA4cjVnZ3oxdDYifQ.r2Dsocg7BY9fcbu_JCeYRQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -157,28 +157,37 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.className = 'restaurant-card';
 
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
+  image.className = 'card-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
+  name.className = 'card-title'
   name.innerHTML = restaurant.name;
   li.append(name);
 
+  const cardContent = document.createElement('div');
+  cardContent.className = 'card-content';
+
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  cardContent.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  cardContent.append(address);
+  li.append(cardContent);
 
   const more = document.createElement('a');
+  more.className = 'card-action';
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  
+
+  li.append(more);
 
   return li
 }
